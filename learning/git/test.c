8af71ebbf0f21c4b207e8ce178c3d180bc6fc30a
kalaiselvan.g <kalaiselvan.g@zilogic.com>
2023-04-20 23:21:59 +0530

    test_file
    this is to save changes

---
+++ learning/git/test.c
@@ -0,0 +1,19 @@
+//this is a testcode
+
+#include <stdio.h>
+
+void add(int a, int b)
+{
+	printf("the sum is %d", a+b);
+}
+
+int main()
+{
+	int a = 5, b = 6;
+
+	printf("Hello,world\n");
+
+	add(a, b);
+
+	return 0;
+}
